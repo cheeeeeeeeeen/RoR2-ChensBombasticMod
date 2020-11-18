3ba4b7e83c22cdb667196923a351e77e4c02e2db
cheeeeeeeeeen <blancfaye7@gmail.com>
2020-11-19 03:01:17 +0800

    Initialize repo and implement Malice

---
+++ Artifacts/Malice.cs
@@ -0,0 +1,94 @@
+﻿using RoR2;
+using RoR2.Artifacts;
+using System.Collections.Generic;
+using TILER2;
+using UnityEngine;
+using static RoR2.Artifacts.BombArtifactManager;
+
+namespace Chen.Bombaholic
+{
+    public class Malice : Artifact_V2<Malice>
+    {
+        public override string displayName => "Artifact of Malice";
+
+        protected override string GetNameString(string langid = null) => displayName;
+
+        protected override string GetDescString(string langid = null) => "Enemies drop multiple exploding bombs on spawn.";
+
+        public Malice()
+        {
+            iconResourcePath = "@ChensBombasticMod:Assets/Bombastic/texMaliceOn.png";
+            iconResourcePathDisabled = "@ChensBombasticMod:Assets/Bombastic/texMaliceOff.png";
+        }
+
+        public override void Install()
+        {
+            base.Install();
+            CharacterBody.onBodyStartGlobal += CharacterBody_onBodyStartGlobal;
+            Run.onRunStartGlobal += Run_onRunStartGlobal;
+        }
+
+        public override void Uninstall()
+        {
+            base.Uninstall();
+            CharacterBody.onBodyStartGlobal -= CharacterBody_onBodyStartGlobal;
+            Run.onRunStartGlobal -= Run_onRunStartGlobal;
+        }
+
+        private void Run_onRunStartGlobal(Run obj)
+        {
+            if (IsActiveAndEnabled()) Run.instance.gameObject.AddComponent<MaliceManager>();
+        }
+
+        private void CharacterBody_onBodyStartGlobal(CharacterBody obj)
+        {
+            if (!IsActiveAndEnabled() || obj.teamComponent.teamIndex != TeamIndex.Monster) return;
+            MaliceManager maliceManager = Run.instance.GetComponent<MaliceManager>();
+            if (!maliceManager) return;
+
+            Vector3 corePosition = obj.corePosition;
+            float bombComputation = obj.bestFitRadius * extraBombPerRadius * cvSpiteBombCoefficient.value;
+            int num = Mathf.Min(maxBombCount, Mathf.CeilToInt(bombComputation));
+            for (int i = 0; i < num; i++)
+            {
+                Vector3 b = Random.insideUnitSphere * (bombSpawnBaseRadius + obj.bestFitRadius * bombSpawnRadiusCoefficient);
+                BombRequest item = new BombRequest
+                {
+                    spawnPosition = corePosition,
+                    raycastOrigin = corePosition + b,
+                    bombBaseDamage = obj.damage * bombDamageCoefficient,
+                    attacker = obj.gameObject,
+                    teamIndex = obj.teamComponent.teamIndex,
+                    velocityY = Random.Range(5f, 25f)
+                };
+                maliceManager.bombRequestQueue.Enqueue(item);
+            }
+        }
+    }
+
+    public class MaliceManager : MonoBehaviour
+    {
+        private Malice maliceInst;
+
+        public readonly Queue<BombRequest> bombRequestQueue = new Queue<BombArtifactManager.BombRequest>();
+
+        private void Awake()
+        {
+            maliceInst = Malice.instance;
+        }
+
+        private void FixedUpdate()
+        {
+            if (maliceInst.IsActiveAndEnabled() && bombRequestQueue.Count > 0)
+            {
+                BombRequest bombRequest = bombRequestQueue.Dequeue();
+                Ray ray = new Ray(bombRequest.raycastOrigin + new Vector3(0f, maxBombStepUpDistance, 0f), Vector3.down);
+                float maxDistance = maxBombStepUpDistance + maxBombFallDistance;
+                if (Physics.Raycast(ray, out RaycastHit raycastHit, maxDistance, LayerIndex.world.mask, QueryTriggerInteraction.Ignore))
+                {
+                    SpawnBomb(bombRequest, raycastHit.point.y);
+                }
+            }
+        }
+    }
+}
---
+++ Bombastic.cs
@@ -0,0 +1,89 @@
+﻿#undef DEBUG
+
+using BepInEx;
+using BepInEx.Configuration;
+using R2API;
+using R2API.Utils;
+using System.Reflection;
+using TILER2;
+using UnityEngine;
+using static TILER2.MiscUtil;
+using Path = System.IO.Path;
+
+namespace Chen.BombasticMod
+{
+    [BepInPlugin(ModGuid, ModName, ModVer)]
+    [BepInDependency(R2API.R2API.PluginGUID, R2API.R2API.PluginVersion)]
+    [BepInDependency(TILER2Plugin.ModGuid, TILER2Plugin.ModVer)]
+    [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.EveryoneNeedSameModVersion)]
+    [R2APISubmoduleDependency(nameof(ResourcesAPI))]
+    public class BombasticPlugin : BaseUnityPlugin
+    {
+        public const string ModVer =
+#if DEBUG
+            "0." +
+#endif
+            "1.0.0";
+
+        public const string ModName = "ChensBombasticMod";
+        public const string ModGuid = "com.Chen.ChensBombasticMod";
+
+        private static ConfigFile cfgFile;
+
+        internal static FilingDictionary<CatalogBoilerplate> chensItemList = new FilingDictionary<CatalogBoilerplate>();
+
+        internal static BepInEx.Logging.ManualLogSource _logger;
+
+        private void Awake()
+        {
+            _logger = Logger;
+            
+#if DEBUG
+            Log.Warning("Running test build with debug enabled! Report to CHEN if you're seeing this!");
+            On.RoR2.Networking.GameNetworkManager.OnClientConnect += (self, user, t) => { };
+#endif
+
+            Log.Debug("Loading assets...");
+            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("ChensBombasticMod.chensbombasticmod_assets"))
+            {
+                var bundle = AssetBundle.LoadFromStream(stream);
+                var provider = new AssetBundleResourcesProvider("@ChensBombasticMod", bundle);
+                ResourcesAPI.AddProvider(provider);
+            }
+
+            cfgFile = new ConfigFile(Path.Combine(Paths.ConfigPath, ModGuid + ".cfg"), true);
+
+            Log.Debug("Instantiating item classes...");
+            chensItemList = T2Module.InitAll<CatalogBoilerplate>(new T2Module.ModInfo
+            {
+                displayName = "Chen's Bombastic Mod",
+                longIdentifier = "ChensBombasticMod",
+                shortIdentifier = "CBM",
+                mainConfigFile = cfgFile
+            });
+
+            T2Module.SetupAll_PluginAwake(chensItemList);
+            T2Module.SetupAll_PluginStart(chensItemList);
+        }
+
+        private void Start()
+        {
+            CatalogBoilerplate.ConsoleDump(Logger, chensItemList);
+        }
+    }
+
+    public static class Log
+    {
+        public static void Debug(object data) => logger.LogDebug(data);
+
+        public static void Error(object data) => logger.LogError(data);
+
+        public static void Info(object data) => logger.LogInfo(data);
+
+        public static void Message(object data) => logger.LogMessage(data);
+
+        public static void Warning(object data) => logger.LogWarning(data);
+
+        public static BepInEx.Logging.ManualLogSource logger => BombasticPlugin._logger;
+    }
+}
